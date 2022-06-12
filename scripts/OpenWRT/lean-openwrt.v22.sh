8c074f59ff60724008b115f4fad3815039fcc213
gilagajet <84836081+gilagajet@users.noreply.github.com>
2022-06-13 03:55:17 +0800

    Update lean-openwrt.v22.sh

--- scripts/OpenWRT/lean-openwrt.v22.sh
+++ scripts/OpenWRT/lean-openwrt.v22.sh
@@ -2,16 +2,18 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: gilagajet
 #=================================================
+
+#-----------------------------------------------------------------------------
+
+###Base
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/OpenWRT/g' package/base-files/files/bin/config_generate
-
 
 # Add kernel build user
 sed -i 's/CONFIG_KERNEL_BUILD_USER=""/CONFIG_KERNEL_BUILD_USER="gilagajet"/g' .config
@@ -37,26 +39,6 @@ sed -i 's/1.openwrt.pool.ntp.org/time.cloudflare.com/g' package/base-files/files
 sed -i 's/2.openwrt.pool.ntp.org/clock.sjc.he.net/g' package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/my.pool.ntp.org/g' package/base-files/files/bin/config_generate
 
-#-----------------------------------------------------------------------------
-
-# HelmiWrt packages
-pushd package
-git clone --depth=1 https://github.com/helmiau/helmiwrt-packages
-popd
-
-# Add themes from kenzok8 openwrt-packages
-pushd package
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new kenzok8/luci-theme-atmaterial_new
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge kenzok8/luci-theme-edge
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-ifit kenzok8/luci-theme-ifit
-
-svn co https://github.com/immortalwrt/luci/trunk/themes/luci-theme-bootstrap-mod
-
-popd
-
-#-----------------------------------------------------------------------------
-
-
 # Mod zzz-default-settings
 #pushd package/lean/default-settings/files
 #sed -i '/http/d' zzz-default-settings
@@ -69,23 +51,28 @@ popd
 #sed -i "s/Shanghai/Kuala Lumpur/g" zzz-default-settings
 #popd
 
-# Fix mt76 wireless driver
-pushd package/kernel/mt76
-sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+#-----------------------------------------------------------------------------
+
+###Themes
+
+# Add themes from kenzok8 openwrt-packages
+pushd package
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new kenzok8/luci-theme-atmaterial_new
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge kenzok8/luci-theme-edge
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-ifit kenzok8/luci-theme-ifit
+svn co https://github.com/immortalwrt/luci/trunk/themes/luci-theme-bootstrap-mod luci-theme-bootstrap-mod
+
 popd
 
 #-----------------------------------------------------------------------------
 
-# Add luci-app-ssr-plus
-pushd package
-git clone --depth=1 https://github.com/fw876/helloworld
-#[ ! -d helloworld/luci-app-ssr-plus ] && svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-ssr-plus helloworld/luci-app-ssr-plus
-popd
+###Module/Apps
 
-# Add luci-app-passwall from kenzok
+# Add luci-app-ssr-plus (subversion old ssr-plus)
 pushd package
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages
-git clone --depth=1 https://github.com/kenzok8/small-package
+#git clone --depth=1 https://github.com/fw876/helloworld
+#svn co https://github.com/NueXini/NueXini_Packages/branches/v20220412/luci-app-ssr-plus
+svn co https://github.com/NueXini/NueXini_Packages/tags/v20220412/luci-app-ssr-plus
 popd
 
 # Add luci-app-passwall
@@ -94,6 +81,12 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 [ ! -d openwrt-passwall/luci-app-passwall ] && svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-passwall openwrt-passwall/luci-app-passwall
 sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
 grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
+popd
+
+# Extra dependency
+pushd package
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+git clone --depth=1 https://github.com/kenzok8/small-package
 popd
 
 # Add OpenClash
@@ -113,13 +106,24 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon
 popd
 
-#-----------------------------------------------------------------------------
 # Zram Source from ImmortalWRT
 pushd package/system
 rm -r zram-swap
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/system/zram-swap 
 popd
 
+#-----------------------------------------------------------------------------
+
+###Patch
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
+#-----------------------------------------------------------------------------
+
+###Extra
 
 #upx
 git clone --depth=1 https://github.com/kuoruan/openwrt-upx.git /workdir/openwrt/staging_dir/host/bin/upx
